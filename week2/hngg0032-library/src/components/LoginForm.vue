ea77a58e04092cdb0d5cd1ba95d38d00e4a8e393
hngg0032 <hngg0032@student.monash.edu>
2024-08-16 15:14:54 +1000

    Added DataTable into the form

--- week2/hngg0032-library/src/components/LoginForm.vue
+++ week2/hngg0032-library/src/components/LoginForm.vue
@@ -94,25 +94,13 @@
           </div>
         </form>
         <div class="row mt-5" v-if="submittedCards.length">
-          <div class="d-flex flex-wrap justify-content-start">
-            <div
-              v-for="(card, index) in submittedCards"
-              :key="index"
-              class="card m-2"
-              style="width: 18rem"
-            >
-              <div class="card-header">User Information</div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">Username: {{ card.username }}</li>
-                <li class="list-group-item">Password: {{ card.password }}</li>
-                <li class="list-group-item">
-                  Australian Resident: {{ card.isAustralian ? 'Yes' : 'No' }}
-                </li>
-                <li class="list-group-item">Gender: {{ card.gender }}</li>
-                <li class="list-group-item">Reason: {{ card.reason }}</li>
-              </ul>
-            </div>
-          </div>
+          <DataTable :value="submittedCards">
+            <Column field="username" header="Username"></Column>
+            <Column field="password" header="Password"></Column>
+            <Column field="isAustralian" header="Australian Resident"></Column>
+            <Column field="gender" header="Gender"></Column>
+            <Column field="reason" header="Reason"></Column>
+          </DataTable>
         </div>
       </div>
     </div>
@@ -121,8 +109,8 @@
 
 <script setup>
 import { ref } from 'vue'
-// import DataTable from 'primevue/datatable';
-// import Column from 'primevue/column';
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
 
 const formData = ref({
   username: '',
